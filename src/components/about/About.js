87afbc14e07fa9d30a0d289beb75a6625e91845c
TEONNA ZARAGOZA <teonna.zaragoza@gmail.com>
2019-03-17 16:12:29 -0700

    about html

---
+++ src/components/about/About.js
@@ -0,0 +1,30 @@
+import React from 'react';
+
+function About() {
+  return (
+    <>
+      <header>
+        <h2>TEONNA ZARAGOZA</h2>
+        <h4>software developer</h4>
+        <h4>nurse</h4>
+        <h4>ultra runner</h4>
+      </header>
+      <body>
+        <section>
+          <h3> /bio</h3>
+          <p>I am an Oregon native with a passion for outdoor adventures and pushing your to the other side of your physical limits. Apart from running, I am passionate about serving those in my community and empowering the future.</p>
+        </section>
+        <section>
+          <h3>/hobbies</h3>
+          <ul>
+            <li>Running</li>
+            <li>Coaching cross-country</li>
+            
+          </ul>
+        </section>
+      </body>
+    </>
+  );
+}
+
+export default About;
