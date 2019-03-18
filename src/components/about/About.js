91ee0057f1f4c92f0c98c8404ffef37777ce9c9d
TEONNA ZARAGOZA <teonna.zaragoza@gmail.com>
2019-03-17 20:54:47 -0700

    css for home header and nav

--- src/components/about/About.js
+++ src/components/about/About.js
@@ -9,7 +9,7 @@ function About() {
         <h4>nurse</h4>
         <h4>ultra runner</h4>
       </header>
-      <body>
+      <main>
         <section>
           <h3> /bio</h3>
           <p>I am an Oregon native with a passion for outdoor adventures and pushing your to the other side of your physical limits. Apart from running, I am passionate about serving those in my community and empowering the future.</p>
@@ -19,10 +19,10 @@ function About() {
           <ul>
             <li>Running</li>
             <li>Coaching cross-country</li>
-            
+
           </ul>
         </section>
-      </body>
+      </main>
     </>
   );
 }
--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -7,27 +7,34 @@ import {
 } from 'react-router-dom';
 import Home from '../home/Home';
 import About from '../about/About';
+import styles from './Header.css';
 
 function Header() {
   return (
     <>
-    <header>
-      <a href='https://www.linkedin.com/in/teonnazaragoza/' target="_blank">Linked icon</a>
-      <a href='https://github.com/tmzaragoza' target="_blank">Github icon</a>
-      <a href='https://www.instagram.com/t_on_a/' target="_blank">instagram icon</a>
-    </header>
-      <Router>
-        <nav>
-          {/* <Link to='/'></Link> */}
-          <Link to='/projects'>PROJECTS</Link>
-          <Link to='/future'>FUTURE</Link>
-          <Link to='/about'>ABOUT</Link>
-        </nav>
-        <Switch>
-          <Route exact path='/' component={Home} />
-          <Route exact path='/about' component={About} />
-        </Switch>
-      </Router>
+      <header className={styles.Header}>
+        <header className={styles.header}>
+          <a href="teonna.dev" className={styles.TZ}>TZ</a>
+          <a href='https://www.linkedin.com/in/teonnazaragoza/' target="_blank">
+            <img src="../../../images/linkedin.png" alt="linkedIn logo"/>
+          </a>
+          <a href='https://github.com/tmzaragoza' target="_blank">
+            <img src="../../../images/github.png" alt="github logo" />
+          </a>
+        </header>
+        <Router>
+          <nav className={styles.nav}>
+            {/* <Link to='/'></Link> */}
+            <Link to='/projects'>/WORK</Link>
+            <Link to='/future'>/FUTURE</Link>
+            <Link to='/about'>/ABOUT</Link>
+          </nav>
+          <Switch>
+            <Route exact path='/' component={Home} />
+            <Route exact path='/about' component={About} />
+          </Switch>
+        </Router>
+      </header>
     </>
   );
 }
