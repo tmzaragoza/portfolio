fd0ee7fc8e3d5cee4fc3172fc10106e8f44cb56b
TEONNA ZARAGOZA <teonna.zaragoza@gmail.com>
2019-03-19 22:01:29 -0700

    removed projects into services file

--- src/components/projects/Projects.js
+++ src/components/projects/Projects.js
@@ -2,19 +2,9 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import Project from './Project';
 
-const text = [
-  {
-    title: 'SimAlchemy',
-    date: '02/19',
-    concept: 'CLI game',
-    about: 'How does one make a database fun? We chose to make a code school adventure game in the command line to show case our understand of a NOSQL database.',
-    tech: 'Express, MongoDB, Node.js',
-    pic: 'pic'
-  }
-];
-function Projects({ projects = text }) {
-  const listOfProjects = projects.map(project => {
-    return <li key={project.name}><Project /></li>;
+function Projects({ projects }) {
+  const listOfProjects = projects.map((project, id) => {
+    return <li key={id}><Project project={project}/></li>;
   });
 
   return (
