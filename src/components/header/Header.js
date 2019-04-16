0b90aa16d05ed5176ce8200c71ce2cf2ba6b0a1b
TEONNA ZARAGOZA <teonna.zaragoza@gmail.com>
2019-04-16 14:00:52 -0700

    landing page the way I want.

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,29 +1,25 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import styles from './Header.css';
-import Projects from '../projects/Projects';
-import About from '../about/About';
 
 function Header() {
   return (
-    <>
-      <header className={styles.Header}>
-        <section className={styles.section}>
-          <Link href="teonna.dev" className={styles.TZ}>TZ</Link>
-          <a href='https://www.linkedin.com/in/teonnazaragoza/' target="_blank">
-            <img src="../../../images/linkedin.png" alt="linkedIn logo" />
-          </a>
-          <a href='https://github.com/tmzaragoza' target="_blank">
-            <img src="../../../images/github.png" alt="github logo" />
-          </a>
-        </section>
-        <nav className={styles.nav}>
-          <Link to='/projects'>/WORK</Link>
-          <Link to='/future'>/FUTURE</Link>
-          <Link to='/about'>/ABOUT</Link>
-        </nav>
-      </header>
-    </>
+    <header className={styles.Header}>
+      <section className={styles.section}>
+        <Link to='/' className={styles.TZ}>TZ</Link>
+        <a href='https://www.linkedin.com/in/teonnazaragoza/' target="_blank">
+          <img src="../../../images/linkedin.png" alt="linkedIn logo" />
+        </a>
+        <a href='https://github.com/tmzaragoza' target="_blank">
+          <img src="../../../images/github.png" alt="github logo" />
+        </a>
+      </section>
+      <nav className={styles.nav}>
+        <Link to='/projects'>/WORK</Link>
+        <Link to='/future'>/FUTURE</Link>
+        <Link to='/about'>/ABOUT</Link>
+      </nav>
+    </header>
   );
 }
 export default Header;
